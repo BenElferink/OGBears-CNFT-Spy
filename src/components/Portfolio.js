012498bba4640c1ba5a673b08ab16dbd730433d2
Ben Elferink <ben.elferink@icloud.com>
2021-12-31 22:02:19 +0200

    fix: NaN is a feature 😉

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -186,7 +186,7 @@ function Portfolio({ floorData }) {
               </span>
               <div className='flex-col' style={{ marginLeft: '0.5rem' }}>
                 <ChangeGreenRed
-                  value={((100 / totalBalance ? totalBalance : 100) * (totalBalance - totalPayed)).toFixed(0)}
+                  value={((100 / totalBalance) * (totalBalance - totalPayed)).toFixed(0)}
                   suffix='%'
                   invert
                   withCaret
