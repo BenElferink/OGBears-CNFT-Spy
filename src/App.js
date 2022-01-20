aca9b0697490a01fd1016f2d3fe8c9eb73c9a032
Ben Elferink <ben.elferink@icloud.com>
2022-01-20 08:23:36 +0200

    fix: floor data from URL

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import { useEffect, useState } from 'react'
+import Axios from 'axios'
 import { useMediaQuery } from '@mui/material'
 import Listings from './components/Listings'
 import Main from './components/Main'
 import bearsJsonFile from './data/bears'
-import floorSnapshots from './data/floor-data'
 import getCurrentFloorData from './data/functions/getCurrentFloorData'
 
 function App() {
-  const [floorData, setFloorData] = useState(floorSnapshots)
   const isDesktop = useMediaQuery('(min-width: 1024px)')
+  const [floorData, setFloorData] = useState(null)
 
   useEffect(() => {
-    getCurrentFloorData(bearsJsonFile)
-      .then((obj) =>
-        setFloorData((prev) => {
-          const newState = { ...prev }
+    Axios.get(
+      'https://raw.githubusercontent.com/belferink1996/og-bears-cnft-spy/main/src/data/floor-data.json',
+    )
+      .then((response) => {
+        console.log(response)
+        setFloorData(response.data)
 
-          Object.entries(obj).forEach(([key, val]) => {
-            newState[key].push({ ...val, timestamp: 'LIVE' })
-          })
+        getCurrentFloorData(bearsJsonFile)
+          .then((obj) =>
+            setFloorData((prev) => {
+              const newState = { ...prev }
 
-          return newState
-        }),
-      )
+              Object.entries(obj).forEach(([key, val]) => {
+                newState[key].push({ ...val, timestamp: 'LIVE' })
+              })
+
+              return newState
+            }),
+          )
+          .catch((error) => console.error(error))
+      })
       .catch((error) => console.error(error))
   }, [])
 
