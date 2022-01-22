0b6444a6078e2d9bf2a9a4de8f632bfe2767393e
Ben Elferink <ben.elferink@icloud.com>
2022-01-22 15:25:41 +0200

    feat: make logo clickable

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,12 @@ function Main() {
   return (
     <main className='main'>
       <header className='flex-evenly'>
-        <img src={logo} alt='logo' style={{ maxWidth: '80vw' }} />
+        <img
+          src={logo}
+          alt='logo'
+          style={{ maxWidth: '80vw', cursor: 'pointer' }}
+          onClick={() => window.open('https://ogbears.com', '_blank')}
+        />
       </header>
 
       {floorData ? <Charts /> : <Loading />}
