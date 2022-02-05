fa1bdd4f5e75cd6468abccf0035637fe827f69f4
Ben Elferink <ben.elferink@icloud.com>
2022-02-05 13:53:37 +0200

    feat: new minted bears added

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
 import '../styles/index.css'
-// import { ScreenSizeProvider } from '../contexts/ScreenSizeContext'
-// import { DataProvider } from '../contexts/DataContext'
+import { ScreenSizeProvider } from '../contexts/ScreenSizeContext'
+import { DataProvider } from '../contexts/DataContext'
 
 function MyApp({ Component, pageProps }) {
-  // return (
-  //   <ScreenSizeProvider>
-  //     <DataProvider>
-  //       <Component {...pageProps} />
-  //     </DataProvider>
-  //   </ScreenSizeProvider>
-  // )
-
-  return <Component {...pageProps} />
+  return (
+    <ScreenSizeProvider>
+      <DataProvider>
+        <Component {...pageProps} />
+      </DataProvider>
+    </ScreenSizeProvider>
+  )
 }
 
 export default MyApp
