013bec631e7980015b146c6234bbe1e69f1df846
Ben Elferink <ben.elferink@icloud.com>
2022-02-05 17:20:29 +0200

    feat: display more on scroll (image load heavy)

--- components/Listings.js
+++ components/Listings.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
+import { useScreenSize } from '../contexts/ScreenSizeContext'
 import { useData } from '../contexts/DataContext'
 import { Typography } from '@mui/material'
 import Loading from './Loading'
 import ListItem from './ListItem'
 
 function Listings({ title = 'Recently Sold', sold = true }) {
+  const { isDesktop } = useScreenSize()
   const { getCnftItems, getJpgItems } = useData()
+
   const [data, setData] = useState([])
+  const [displayNum, setDisplayNum] = useState(10)
 
   useEffect(() => {
     getJpgItems({ sold }).then((jpgItems) => {
@@ -16,6 +20,16 @@ function Listings({ title = 'Recently Sold', sold = true }) {
       })
     })
   }, []) // eslint-disable-line
+
+  const handleScroll = (e) => {
+    const { scrollTop, scrollHeight, clientHeight, scrollLeft, clientWidth, scrollWidth } = e.target
+    const isScrolledToBottom = Math.floor(scrollTop) === Math.floor(scrollHeight - clientHeight)
+    const isScrolledToRight = Math.floor(scrollLeft) === Math.floor(scrollWidth - clientWidth)
+
+    if ((isDesktop && isScrolledToBottom) || (!isDesktop && isScrolledToRight)) {
+      setDisplayNum((prev) => prev + 10)
+    }
+  }
 
   return (
     <div className='listings-container'>
@@ -32,18 +46,20 @@ function Listings({ title = 'Recently Sold', sold = true }) {
         {title}
       </Typography>
 
-      <div className='scroll listings'>
+      <div className='scroll listings' onScroll={handleScroll}>
         {data.length ? (
-          data.map(({ assetId, name, price, imageUrl, itemUrl, store, date }) => (
-            <ListItem
-              key={`${store}-${sold ? 'sold' : 'listed'}-${assetId}-${date}`}
-              name={name}
-              price={price}
-              imageSrc={imageUrl}
-              itemUrl={itemUrl}
-              spanArray={[store, `${sold ? 'Sold:' : 'Listed:'} ${new Date(date).toLocaleString()}`]}
-            />
-          ))
+          data.map(({ assetId, name, price, imageUrl, itemUrl, store, date }, idx) =>
+            idx < displayNum ? (
+              <ListItem
+                key={`${store}-${sold ? 'sold' : 'listed'}-${assetId}-${date}`}
+                name={name}
+                price={price}
+                imageSrc={imageUrl}
+                itemUrl={itemUrl}
+                spanArray={[store, `${sold ? 'Sold:' : 'Listed:'} ${new Date(date).toLocaleString()}`]}
+              />
+            ) : null,
+          )
         ) : (
           <Loading />
         )}
