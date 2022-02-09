8ab459d71c0470471ab1087d10eddf98f1989d52
Ben Elferink <ben.elferink@icloud.com>
2022-02-09 22:20:00 +0200

    feat: pull only new assets

--- _bots/blockfrost.js
+++ _bots/blockfrost.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv')
 const fs = require('fs')
 const Axios = require('axios')
-const { BEARS_POLICY_ID } = require('../constants/policy-ids.js')
+const { BEARS_POLICY_ID } = require('../constants/policy-ids')
+const blockfrostJsonFile = require('../data/blockfrost')
 
 dotenv.config()
 
@@ -10,13 +11,14 @@ const BLOCKFROST_KEY = process.env.BLOCKFROST_KEY ?? ''
 const BLOCKFROST_API = 'https://cardano-mainnet.blockfrost.io/api/v0'
 
 const run = async () => {
-  try {
-    const policyAssets = []
-    const populatedAssets = []
+  const policyAssets = []
+  const populatedAssets = blockfrostJsonFile?.assets ?? []
 
+  try {
     console.log('getting all assets from blockfrost')
     for (let page = 1; true; page++) {
       console.log(`querying page number ${page}`)
+
       const { data: policyAssetsPagination } = await Axios.get(
         `${BLOCKFROST_API}/assets/policy/${OGB_POLICY_ID}?page=${page}`,
         {
@@ -26,7 +28,6 @@ const run = async () => {
         },
       )
 
-      console.log(`queried ${policyAssetsPagination.length} assets for page ${page}`)
       if (!policyAssetsPagination.length) {
         break
       }
@@ -37,31 +38,32 @@ const run = async () => {
     }
 
     console.log(`got a total of ${policyAssets.length} assets from blockfrost`)
-    console.log('populating each individual asset from blockfrost')
+    console.log('populating new assets from blockfrost')
 
     for (let idx = 0; idx < policyAssets.length; idx++) {
       const { asset } = policyAssets[idx]
-      console.log(`populating: index ${idx}, asset ${asset}`)
 
-      populatedAssets.push(
-        (
-          await Axios.get(`${BLOCKFROST_API}/assets/${asset}`, {
-            headers: {
-              project_id: BLOCKFROST_KEY,
-            },
-          })
-        ).data,
-      )
+      if (!populatedAssets.find((item) => (item.asset === asset))) {
+        console.log(`idx: ${idx}, populating new asset ${asset}`)
+        
+        const { data: populatedAsset } = await Axios.get(`${BLOCKFROST_API}/assets/${asset}`, {
+          headers: {
+            project_id: BLOCKFROST_KEY,
+          },
+        })
+
+        populatedAssets.push(populatedAsset)
+      }
     }
 
-    console.log('sorting assets by bear #')
+    console.log('sorting assets by bear #ID')
     populatedAssets.sort(
       (a, b) =>
         Number(a.onchain_metadata.name.replace('BEAR', '')) -
         Number(b.onchain_metadata.name.replace('BEAR', '')),
     )
 
-    console.log('saving all assets to JSON file')
+    console.log(`saving ${populatedAssets.length} assets to JSON file`)
     fs.writeFileSync(
       './data/blockfrost.json',
       JSON.stringify({
